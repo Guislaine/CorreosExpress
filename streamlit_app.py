d53ffbd48196b7b6ebb2ebb47dc91b2f15b8df0e
Guislaine <145041564+Guislaine@users.noreply.github.com>
2024-04-11 08:09:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import subprocess
+
+# Titre de l'application
+st.title("Interface pour Calculate flows and map")
+
+# Bouton pour lancer l'exécution du script
+if st.button("Lancer l'exécution du script"):
+    # Exécution du script
+    subprocess.run(["python", "Calculate_flows_and_map.py"])
+    # Message de confirmation
+    st.write("L'exécution du script est terminée !")
 
